cd702532fa0b133cc29c0b42162e74b7787159c4
Jay Bednar <ironviralmedia@gmail.com>
2018-01-18 18:53:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,9 +12,16 @@ function frameIt() {
   });
 }
 
+function submitIt(){
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
 
 getIt();
 frameIt();
+submitIt();
 
 });
